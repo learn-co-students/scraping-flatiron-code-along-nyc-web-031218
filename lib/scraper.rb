d76f3ea29a1c72c829135339315fcf87099dd768
mmcneeley <mmcneeley@gmail.com>
2018-03-17 16:13:56 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,42 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def get_page
+    Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
+
+    #Offerings = doc.css(".post")
+    #Title = doc.css(".post").first.css("h2").text
+    #Title_As_Array = doc.css(".post")[0].css("h2").text
+    #Schedule = doc.css(".post").first.css(".date").text
+    #Description doc.css('.post').first.css('p').text
+
+    # doc.css(".post").each do |post|
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    # end
+  end
+
+  def get_courses
+    self.get_page.css('.post')
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +46,8 @@ class Scraper
       end
     end
   end
-  
+
 end
 
-
-
+#Scraper.new.get_page
+Scraper.new.print_courses
